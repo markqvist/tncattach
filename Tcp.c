41086b2e0cc9c87579844be88749fb8ac2caf308
Valentin Saugnier <valentin.s.10@gmail.com>
2020-06-16 21:55:25 +0200

    :sparkles: Add TCP Kiss

---
+++ Tcp.c
@@ -0,0 +1,36 @@
+#include "Tcp.h"
+
+int open_tcp(char* ip, int port) {
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0) {
+        perror("ERROR opening socket");
+        exit(1);
+    }
+
+    struct hostent *server;
+    struct sockaddr_in serv_addr;
+
+    server = gethostbyname(ip);
+
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+    serv_addr.sin_port = htons(port);
+
+    if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("ERROR connecting");
+        exit(1);
+    }
+
+    return sockfd;
+}
+
+int close_tcp(int fd) {
+	return close(fd);
+}
