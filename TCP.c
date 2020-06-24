26f1e48b192951c1724d93d159007f498befb32c
Mark Qvist <mark@adepta.io>
2020-06-24 14:00:23 +0200

    Renamed TCP files

---
+++ TCP.c
@@ -0,0 +1,37 @@
+#include "TCP.h"
+
+int open_tcp(char* ip, int port) {
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0) {
+        perror("Could not open AF_INET socket");
+        exit(1);
+    }
+
+    struct hostent *server;
+    struct sockaddr_in serv_addr;
+
+    server = gethostbyname(ip);
+
+    if (server == NULL) {
+        perror("Error resolving host");
+        exit(1);
+    }
+
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+    serv_addr.sin_port = htons(port);
+
+    if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Could not connect TCP socket");
+        exit(1);
+    }
+
+    return sockfd;
+}
+
+int close_tcp(int fd) {
+	return close(fd);
+}
---
+++ TCP.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+
+int open_tcp(char* ip, int port);
+int close_tcp(int fd);
