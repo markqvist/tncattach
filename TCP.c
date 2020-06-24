671ea5dda05358423a24e5f1591369f93244ca96
Mark Qvist <mark@adepta.io>
2020-06-24 14:28:50 +0200

    Fixed deprecated bzero and bcopy calls

--- TCP.c
+++ TCP.c
@@ -11,17 +11,15 @@ int open_tcp(char* ip, int port) {
     struct hostent *server;
     struct sockaddr_in serv_addr;
 
-    server = gethostbyname(ip);
-
     if (server == NULL) {
         perror("Error resolving host");
         exit(1);
     }
 
-    bzero((char *) &serv_addr, sizeof(serv_addr));
+    memset(&serv_addr, 0, sizeof(serv_addr)); 
     serv_addr.sin_family = AF_INET;
 
-    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+    memcpy(server->h_addr, &serv_addr.sin_addr.s_addr, server->h_length);
     serv_addr.sin_port = htons(port);
 
     if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
